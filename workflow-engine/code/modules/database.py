39f16e23870818fc9250036e11756e20fee93c58
Eric Moody <eric@whitestarresearch.com>
2024-02-08 22:58:40 +0000

    Started to build the functionality to run actions

--- workflow-engine/code/modules/database.py
+++ workflow-engine/code/modules/database.py
@@ -17,12 +17,32 @@
 
 import yaml
 from pymongo import MongoClient
+from bson import ObjectId, json_util
 import urllib.parse
+import json
+import re
 
 class Database:
+    """
+    This is a class used for accessing the database. When initialize it it open a connection to the database. 
+    The config for the database connection comes from /opt/self-service-portal/conf/db.conf
+
+    Attributes:
+        mongo_client (MongoClient): The client class for the db connection
+        datbase (Database): The database the client is connected to
+        collection (Collection): The collection that the client is connect to
+    """
     conf_home = "/opt/self-service-portal/conf"
 
     def __init__(self, database, collection) -> None:
+        """
+        The constructor for the Database class.
+
+        Parameters:
+            self (Database): The object itself
+            database (Str): The name of the database to connect to
+            collection (Str): The name of the collection to connect to
+        """
         with open(self.conf_home+"/db.yaml",'r') as file:
             db_config = yaml.safe_load(file)
 
@@ -31,3 +51,61 @@ class Database:
         self.mongo_client = MongoClient('mongodb://%s:%s@%s:%s' % (username, password, db_config['host'], db_config['port']))
         self.database = self.mongo_client[database]
         self.collection = self.database[collection]
+
+    def find_by_id(self, object_id):
+        """
+        A method to find a document by its object id. 
+        
+        Parameters:
+            self (Database): The instantiation of the Database class
+            object_id (Str): The id of the document to find. The id must be 24 hexadecimal characters with lowercase letters
+
+        Returns:
+            Dict: A dict with the document if the document is found. 
+            None: If the document is not found.
+        """
+        if not re.match('^[0-9a-f]{24}$',object_id):
+            raise "Object id must be 24 chacters hexadecimal string with lowercase letters"
+
+        object_instance =  ObjectId(object_id)
+        result = self.collection.find_one({"_id": object_instance})
+
+        return json.loads(json_util.dumps(result))
+
+    def find_one_by_query(self, query):
+        """
+        A function to find one record using a query
+
+        Parameters:
+            self (Database): The instantiation of the Database class
+            query (Dict): A dictonary with the query
+        
+        Returns:
+            Dict: A dict with the document if document is found
+            None: If the document is not found        
+        """
+
+        result = self.collection.find_one(query)
+
+        return json.loads(json_util.dumps(result))
+    
+    def insert_document(self, document):
+        """
+        A function to insert a new doument
+
+        Parameters:
+            self (Database): The instantiation of the Database class
+            document (Dict): A dictonary with the document to inset into a collection
+
+        Returns:
+            object_id (Str): A 24 character hexadecmal string that represents the id of the new object
+        """
+
+        result = self.collection.insert_one(document)
+
+        return str(result.inserted_id)
+
+    
+
+
+        
--- workflow-engine/code/runner.py
+++ workflow-engine/code/runner.py
@@ -15,17 +15,197 @@
 #     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
-import json
 import time
 from modules.database import Database
+from flask import abort, request
+import re
+from kubernetes import client, config, utils
+import yaml
+from urllib.parse import urlparse
 
-def result(job_id, runner_result):
+def result(execution_id, runner_result):
     #result_dict = json.loads(runner_result)
 
     runner_result['time'] = int(time.time())
     print(runner_result)
-    print("The job id is: " + job_id)
+    print("The execution id is: " + execution_id)
 
     db_connection = Database("workflow-engine", "runner-result")
     result = db_connection.collection.insert_one(runner_result)
     print("Insert Result: ", result.inserted_id)
+
+def get_execution(execution_id):
+    """
+    The function to get the information about an action execution. It will include the parameters the action should run with and if it has completed the result.
+
+    Return schema:
+    "_id": ObjectId(Str),
+    "job_id": Str,
+    "pod_id": Str,
+    "action_name": Str,
+    "standard_output": Str or None,
+    "error_output": Str or None,
+    "completion_time": Unix timestamp,
+    "parameters": The parameters for the action, this will be a string or object, depending on the action
+
+    Parameters:
+        execution_id (string): A 24 character hexadecimal string with lowercase letters.
+
+    Returns:
+        Dict: A dictory with the action execution information.
+    """
+    if not re.match('^[0-9a-f]{24}$',execution_id):
+        abort(406, "Execution id must be 24 chacters hexadecimal string with lowercase letters")
+
+    db_connection = Database("workflow-engine", "runnerExecution")
+    
+    result = db_connection.find_by_id(execution_id)
+    if result:
+        return result
+    else:
+        abort(404, f"Execution {execution_id} not found")
+
+    print(result)
+
+def get_action_definition(action_namespace,action_name,version):
+    """
+    A function to get the definitin of an action from the database
+    
+    Parameters:
+        action_namespace (Str): The namespace the action resides in
+        action_name (Str): The name of the action
+        version (Int): The version of the action
+    Returns:
+        Dict: A dict with the defination of the action.
+        Schema:
+            "namespace": String,
+            "action_name": String,
+            "version": Int,
+            "container_repo": String,
+            "container_name": String,
+            "container_tag": String,
+            "parameter_schema": None, to be used later
+    """
+    db_connection = Database("workflow-engine", "actionDefinition")
+    query = {"$and": [
+        {"namespace":action_namespace},
+        {"action_name":action_name},
+        {"version":version}
+    ]}
+    result = db_connection.find_one_by_query(query)
+    if result:
+        return result
+    else:
+        raise(f"Action in namespace: {action_namespace}, with name {action_name}, and version {version} not found")
+
+def create_execution_record(action_namespace,action_name,version,parameters,job_id):
+    """
+    A function to create the inital record in the database used for a action execution
+
+    Parameters:
+        action_namespace (Str): The namespace the action resides in
+        action_name (Str): The name of the action
+        version (Int): The version of the action
+        parameters (Object): The parameters used to run the object
+        job_id (Str): The name of the kubernetes job that will run the action
+
+    Returns:
+        execution_id (Str): A 24 character hexadecimal string 
+    """
+    db_connection = Database("workflow-engine", "runnerExecution")
+    document = {
+        "action_namespace": action_namespace,
+        "action_name": action_name,
+        "version": version,
+        "parameters": parameters,
+        "job_id": job_id
+    }
+    
+    execution_id = db_connection.insert_document(document)
+
+    return execution_id
+
+def submit_execution(action_namespace, action_name, version, parameters):
+    """
+    A function to submit an action for execution
+
+    Parameters:
+    action_namespace (Str): The namespace the action resides in
+    action_name (Str): The name of the action
+    parameters (Object): Contans the parameters for the action. This will vary from action to action
+    """
+
+    config.load_kube_config()
+    k8s_client = client.ApiClient()
+
+    job_id =  action_namespace + "-" + action_name + "-" + str(time.time_ns())
+    action_definition = get_action_definition(action_namespace, action_name, version)
+    execution_id = create_execution_record(action_namespace,action_name,version, parameters,job_id)
+
+    #TO-DO Figure out a better way to generate the callback url
+    parsed_url = urlparse(request.base_url)
+    callback_base_url = parsed_url[0] + "://" + parsed_url[1] + "/api/runner/" 
+                
+    #This is not pretty, but better than using a heredoc with some yaml in it.
+    job_dict = {
+        'apiVersion': 'batch/v1',
+        'kind': 'Job',
+        'metadata': { 'name': job_id},
+        'spec': {
+            'template': {
+                'spec': {
+                    'containers': [{
+                            'name': 'action-runner',
+                            'image': f"{action_definition['container_repo']}/{action_definition['container_name']}:{action_definition['container_tag']}",
+                            'env': [
+                                {
+                                    'name': 'RUNNER_ARGS',
+                                    'value': 'I am running in a k8s job'
+                                }, {
+                                    'name': 'POD_ID',
+                                    'valueFrom': {'fieldRef': { 'fieldPath': 'metadata.name'}}
+                                }, {
+                                    'name': 'JOB_ID',
+                                    'value': job_id
+                                }, {
+                                    'name': 'EXECUTION_ID',
+                                    'value': execution_id
+                                }, {
+                                    'name': 'POSTBACK_BASE_URL',
+                                    'value': callback_base_url
+                                }
+                            ]
+                        }
+                    ],
+                    'restartPolicy': 'Never',
+                    'imagePullSecrets': [
+                        {'name': 'regcred'}
+                    ]
+                }
+            },
+            'backoffLimit': 0,
+            'podFailurePolicy': {
+                'rules': [
+                    {
+                        'action': 'FailJob',
+                        'onExitCodes': {
+                            'containerName': 'action-runner',
+                            'operator': 'NotIn',
+                            'values': [0]
+                        }
+                    }, {
+                        'action': 'Ignore',
+                        'onPodConditions': [
+                            {'type': 'DisruptionTarget'}
+                        ]
+                    }
+                ]
+            }
+        }
+    }
+   
+    #narf = utils.create_from_dict(k8s_client, job_dict, namespace="testing")
+    #print(narf)
+
+def do_something():
+    submit_execution("core","echo",1,"ccc")
